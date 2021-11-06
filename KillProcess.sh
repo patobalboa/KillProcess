a8bb7092ba55b2f0bd43af31182b4bbdb071d147
Patricio <pbalboa08@gmail.com>
2021-11-06 05:21:42 -0300

    Versión 1.1

--- KillProcess.sh
+++ KillProcess.sh
@@ -1,8 +1,13 @@
-process=`ps -fe |grep 'IDENTIFY_PROCESS' |grep -v grep`
-  echo "MATANDO $process"
-  for PROCESO in `echo "$process" | awk '{ print $2 }'`
-        do
-            proc_name=`echo "$process" | grep $PROCESO`;
-            kill -9 "$PROCESO";
-            echo "Killed: $proc_name"
-        done;
+  
+  if[$1!=null];then
+    process=`ps -fe |grep "$1" |grep -v grep`
+    echo "MATANDO $process"
+    for PROCESO in `echo "$process" | awk '{ print $2 }'`
+          do
+              proc_name=`echo "$process" | grep $PROCESO`;
+              kill -9 "$PROCESO";
+              echo "Killed: $proc_name"
+          done;
+  else{
+    echo "ERROR: Falta un valor agregado. \nTienes que agregar una Cadena Identificadora, por ejemplo:\n         sh KillProcess.sh procesos.php"
+  }
